c43258783b4f5ef15a3808906910563962e56bee
Patrick Hudson <patrick@hudson.bz>
2017-03-07 17:41:15 -0600

    dockerhub

--- README.md
+++ README.md
@@ -1,1 +1,97 @@
-hai
+# Scripts that upload Docker images to various repositories
+
+## upload_ecr.sh
+
+This script install all necessary tools to build and upload docker images to Amazon ECR.
+
+### Usage:
+
+*IMPORTANT* - On the first run of this script, you *MUST* pass the -f flag to indicate the first run of the script. This installs dependencies and such.
+
+```
+  -h| --help           Displays this help text
+                       this is more help text.
+----------------------------------------------------------------
+REQUIRED
+----------------------------------------------------------------
+  -u| --account-id     AWS Account ID - Found in Account settings in AWS Console
+  -i| --image          Name of your image to upload
+  -t| --tag            Image Tag (Maximum of 1 tag per image)
+----------------------------------------------------------------
+Optional
+----------------------------------------------------------------
+  -r| --region         AWS Region to upload to (Default: us-east-1) 
+  -f| --first-run      Specify this flag if this is the first run of the script (Installs dependencies like Docker/Python/AWSCLI)
+```
+
+#### Example command for first run
+
+``` ./upload_ecr.sh -f -u accountid -i imagename -t tag -r us-east-1 ```
+
+#### Example command to only upload image
+
+``` ./upload_ecr.sh -u accountid -i imagename -t tag -r us-east-1 ```
+
+## upload_dockerhub.sh
+
+This script install all necessary tools to build and upload docker images to DockerHub.
+
+### Usage:
+
+*IMPORTANT* - On the first run of this script, you *MUST* pass the -f flag to indicate the first run of the script. This installs dependencies and such.
+
+```
+  -h| --help           Displays this help text
+                       this is more help text.
+----------------------------------------------------------------
+REQUIRED
+----------------------------------------------------------------
+  -u| --user           DockerHub Username
+  -i| --image          Name of your image to upload
+  -t| --tag            Image Tag (Maximum of 1 tag per image)
+----------------------------------------------------------------
+Optional
+----------------------------------------------------------------
+  -p| --password       DockerHub Password (You can pass the password as an argument if you wish to not authenticate interactively)
+  -f| --first-run      Specify this flag if this is the first run of the script (Installs dependencies like Docker)
+```
+
+#### Example command for first run
+
+``` ./upload_dockerhub.sh -f -u user1 -i imagename -t tag -f ```
+
+#### Example command to only upload image
+
+``` ./upload_dockerhub.sh -u user1 -i imagename -t tag```
+
+## upload_dockerprivate.sh
+
+This script install all necessary tools to build and upload docker images to a Private Repo.
+
+### Usage:
+
+*IMPORTANT* - On the first run of this script, you *MUST* pass the -f flag to indicate the first run of the script. This installs dependencies and such.
+
+```
+  -h| --help           Displays this help text
+                       this is more help text.
+----------------------------------------------------------------
+REQUIRED
+----------------------------------------------------------------
+  -u| --user           DockerHub Username
+  -i| --image          Name of your image to upload
+  -t| --tag            Image Tag (Maximum of 1 tag per image)
+----------------------------------------------------------------
+Optional
+----------------------------------------------------------------
+  -p| --password       DockerHub Password (You can pass the password as an argument if you wish to not authenticate interactively)
+  -f| --first-run      Specify this flag if this is the first run of the script (Installs dependencies like Docker)
+```
+
+#### Example command for first run
+
+``` ./upload_dockerprivate.sh -f -u user1 -i imagename -t tag -f ```
+
+#### Example command to only upload image
+
+``` ./upload_dockerprivate.sh -u user1 -i imagename -t tag```
--- upload_ecr.sh
+++ upload_ecr.sh
@@ -34,9 +34,82 @@ REQUIRED
 Optional
 ----------------------------------------------------------------
   -r| --region         AWS Region to upload to (Default: us-east-1) 
+  -f| --first-run      Specify this flag if this is the first run of the script (Installs dependencies like Docker/Python/AWSCLI)
 EOF
 }
-
+function firstrun(){
+  OS="undetermined"
+  if [ -f /etc/centos-release ]; then
+    if [[ $(awk '{print $3}' /etc/centos-release | cut -d . -f 1) -eq 6 ]]; then 
+      OS="centos6"
+      printf "${G}CentOS 6 detected${N}\n"
+      printf "${G}Installing EPEL if not already installed${N}\n"
+      yum install -y yum-utils >/dev/null 2>&1
+      rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm >/dev/null 2>&1
+    elif [[ $(awk '{print $4}' /etc/centos-release | cut -d . -f 1) -eq 7 ]]; then
+      OS="centos7"
+      printf "${G}CentOS 7 detected${N}\n"
+      printf "${G}Installing EPEL if not already installed${N}\n"
+      yum install -y yum-utils >/dev/null 2>&1
+      rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm >/dev/null 2>&1
+    fi
+  elif [ -f /etc/redhat-release ]; then
+    if [[ $(awk '{print $3}' /etc/redhat-release | cut -d . -f 1) -eq 6 ]]; then 
+      OS="centos6" 
+    elif [[ $(awk '{print $3}' /etc/redhat-release | cut -d . -f 1) -eq 7 ]]; then
+      OS="centos7"
+    fi
+  elif [ -f /etc/os-release ]; then
+    VERSION=$(awk -F= '/^ID=/{print $2}' /etc/os-release)
+    if [[ $VERSION == "ubuntu" ]]; then
+      UVERSION=$(awk -F= '/^VERSION_ID=/{print $2}' /etc/os-release | sed 's/"//g' | cut -d . -f 1)
+      if [[ $UVERSION == "14" ]] || [[ $UVERSION == "16" ]]; then
+        OS="ubuntu"
+      else
+        printf "${R}ERROR: Only Ubuntu 14 and Ubuntu 16 is supported.${N}\n"
+        exit
+      fi
+    fi
+  else
+    echo "OS Seems to be unsupported. Script works with RedHat Enterprise Linux 6 and 7, CentOS 6 and 7, and Ubuntu"
+    exit
+  fi
+  OSREGEX="^(centos)"
+  if [[ $OS =~ $OSREGEX ]]; then
+    OSPKGMAN="yum install -y"
+  else
+    OSPKGMAN="apt-get install -y"
+  fi
+  #Install Python
+  printf "${G}Installing python packages and AWSCLI${N}\n"
+  if [[ $OS == "ubuntu" ]]; then
+    eval $OSPKGMAN docker.io >/dev/null 2>&1
+  elif [[ $OS == "centos6" ]]; then
+    eval $OSPKGMAN docker-io --enablerepo epel >/dev/null 2>&1
+  else
+    eval $OSPKGMAN docker >/dev/null 2>&1
+  fi
+  service docker start
+  eval $OSPKGMAN python-pip >/dev/null 2>&1
+  pip install --upgrade awscli >/dev/null 2>&1
+}
+function uploadImage(){
+  IMAGE=$1
+  TAG=$2
+  AWSID=$3
+  REGION=$4
+  if [[ -z "$REGION" ]]; then
+    REGION="us-east-1"
+  fi
+  if [ ! -f ~/.aws/credentials ]; then
+    printf "${G}AWS Credentials not Found! You will be prompted for your AWS Access Key ID and Secret Access Key${N}\n"
+    aws configure
+  fi
+  eval $(aws ecr get-login --region "$REGION")
+  docker build -t $IMAGE:$TAG .
+  docker tag $IMAGE:$TAG $AWSID.dkr.ecr.$REGION.amazonaws.com/$IMAGE:$TAG
+  docker push $AWSID.dkr.ecr.$REGION.amazonaws.com/$IMAGE:$TAG
+}
 while [ "$1" != "" ]; do
     PARAM=`echo $1 | awk -F= '{print $1}'`
     VALUE=`echo $2 | awk -F= '{print $1}'`
@@ -69,6 +142,10 @@ while [ "$1" != "" ]; do
             AWSID=$VALUE
             shift
             ;;
+        -f | --first-run)
+            firstrun
+            shift
+            ;;
         *)
             echo "ERROR: unknown parameter \"$PARAM\""
             usage
@@ -84,69 +161,6 @@ fi
 if [ -z "$IMAGE" ] || [ -z "$TAG" ]; then
   printf "${R}ERROR: You must define an image name, and tag. Use ./upload_ecr.sh -h for more info${N}\n"
   exit
-fi
-OS="undetermined"
-if [ -f /etc/centos-release ]; then
-  if [[ $(awk '{print $3}' /etc/centos-release | cut -d . -f 1) -eq 6 ]]; then 
-    OS="centos6"
-    printf "${G}CentOS 6 detected${N}\n"
-    printf "${G}Installing EPEL if not already installed${N}\n"
-    yum install -y yum-utils >/dev/null 2>&1
-    rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm >/dev/null 2>&1
-  elif [[ $(awk '{print $4}' /etc/centos-release | cut -d . -f 1) -eq 7 ]]; then
-    OS="centos7"
-    printf "${G}CentOS 7 detected${N}\n"
-    printf "${G}Installing EPEL if not already installed${N}\n"
-    yum install -y yum-utils >/dev/null 2>&1
-    rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm >/dev/null 2>&1
-  fi
-elif [ -f /etc/redhat-release ]; then
-  if [[ $(awk '{print $3}' /etc/redhat-release | cut -d . -f 1) -eq 6 ]]; then 
-    OS="centos6" 
-  elif [[ $(awk '{print $3}' /etc/redhat-release | cut -d . -f 1) -eq 7 ]]; then
-    OS="centos7"
-  fi
-elif [ -f /etc/os-release ]; then
-  VERSION=$(awk -F= '/^ID=/{print $2}' /etc/os-release)
-  if [[ $VERSION == "ubuntu" ]]; then
-    UVERSION=$(awk -F= '/^VERSION_ID=/{print $2}' /etc/os-release | sed 's/"//g' | cut -d . -f 1)
-    if [[ $UVERSION == "14" ]] || [[ $UVERSION == "16" ]]; then
-      OS="ubuntu"
-    else
-      printf "${R}ERROR: Only Ubuntu 14 and Ubuntu 16 is supported.${N}\n"
-      exit
-    fi
-  fi
 else
-  echo "OS Seems to be unsupported. Script works with RedHat Enterprise Linux 6 and 7, CentOS 6 and 7, and Ubuntu"
-  exit
+  uploadImage $IMAGE $TAG $AWSID $REGION
 fi
-OSREGEX="^(centos)"
-if [[ $OS =~ $OSREGEX ]]; then
-  OSPKGMAN="yum install -y"
-else
-  OSPKGMAN="apt-get install -y"
-fi
-#Install Python
-printf "${G}Installing python packages and AWSCLI${N}\n"
-if [[ $OS == "ubuntu" ]]; then
-  eval $OSPKGMAN docker.io >/dev/null 2>&1
-elif [[ $OS == "centos6" ]]; then
-  eval $OSPKGMAN docker-io --enablerepo epel >/dev/null 2>&1
-else
-  eval $OSPKGMAN docker >/dev/null 2>&1
-fi
-service docker start
-eval $OSPKGMAN python-pip >/dev/null 2>&1
-pip install --upgrade awscli >/dev/null 2>&1
-if [[ -z "$REGION" ]]; then
-  REGION="us-east-1"
-fi
-if [ ! -f ~/.aws/credentials ]; then
-  printf "${G}AWS Credentials not Found! You will be prompted for your AWS Access Key ID and Secret Access Key${N}\n"
-  aws configure
-fi
-eval $(aws ecr get-login --region "$REGION")
-docker build -t $IMAGE:$TAG .
-docker tag $IMAGE:$TAG $AWSID.dkr.ecr.$REGION.amazonaws.com/$IMAGE:$TAG
-docker push $AWSID.dkr.ecr.$REGION.amazonaws.com/$IMAGE:$TAG
